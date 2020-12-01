06e024745bd70ccc460f8c785a4cd26e4c1e0c40
renzhenchuan <renzhenchuan@bytedance.com>
2020-12-02 07:01:36 +0800

    fix: 🐛 add print

--- dep/dep_test.go
+++ dep/dep_test.go
@@ -2,8 +2,10 @@ package dep
 
 import (
 	"testing"
+	"fmt"
 )
 
 func TestAnalysis(t *testing.T) {
 	Analysis("github.com/assmdx/gsitg", "../test/gsitg", "../test_result.png")
+	fmt.Println("test finish! Open the test_result.png!")
 }
